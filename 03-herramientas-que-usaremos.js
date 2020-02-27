7284673265646615f030a3bf3441f4f6d74ed2d0
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2020-02-27 18:52:15 -0300

    Clase 03, que herramientas usaremos

---
+++ 03-herramientas-que-usaremos.js
@@ -0,0 +1,8 @@
+/**
+ * Estas son las herramientas que usaremos en el curso
+
+Navegador: Especialmente Chrome ya que cuenta con óptimas herramientas de desarrollo.
+React Developer Tools: Es una herramienta Open Source creada por Facebook y tiene instalación para Chrome o Firefox. Nos dejará ver el código de React inspeccionando elementos.
+Editor de texto: Puedes usar cualquiera, en este curso sugerimos Visual Studio Code. Tiene muchos plugins útiles para el desarrollo.
+Prettier: Es un plugin que hace que el código se vea bien sin importar cómo esta escrito.
+ */
