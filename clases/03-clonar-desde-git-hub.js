b2ba250071cc5efa2e0b9167bd56d842186efc00
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2020-02-28 00:33:10 -0300

    clase 05 - clonando el repositorio oficial del curso desde github

---
+++ clases/03-clonar-desde-git-hub.js
@@ -0,0 +1,32 @@
+/**
+// notas al pie del video 
+
+Clonar el código de GitHub
+En esta clase vamos a comenzar clonando el código del proyecto del repositorio en GitHub.
+
+Es importante que tú y yo tengamos un punto de partida en común. Así vamos a poder asegurarnos que cada cambio que yo haga en el código, tú también lo recibas.
+
+Para hacer esto, en la terminal ve a una carpeta donde quieras que exista el proyecto. Entonces escribes lo siguiente:
+
+$ git clone https://github.com/Sparragus/platzi-badges.git
+Eso va a clonar el repositorio del curso a una carpeta llamada platzi-badges.
+
+Ahora es necesario que te muevas a esa carpeta.
+
+$ cd platzi-badges
+Ahora necesitamos instalar todas las dependencias necesarias para poder correr el proyecto. Lo haremos utilizando npm.
+
+$ npm install
+Este proceso puede tardar un poco. Lo que esta haciendo es descargando todas las bibliotecas de código que el proyecto necesita.
+
+Una vez haya concluido, estamos listos para echar a correr el servidor. Lo hacemos con el comando
+
+$ npm run start
+Cuando el servidor comience, automáticamente va a abrir una pantalla en el navegador con la aplicación.
+
+Si todo salió bien, vas a ver una pantalla que dice “Hello, Platzi Badges”.
+
+En la próxima clase vas a aprender como fue que “Hello, Platzi Badges” llegó desde el código hasta la pantalla de tu navegador.
+
+*Aquí encuentras el repositorio.
+ */
